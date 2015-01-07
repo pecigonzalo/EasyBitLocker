4fa5c7583cc5461036a38b8e8e6fc0e92d3a5187
pecigonzalo <pecigonzalo@outlook.com>
2015-01-08 00:43:21 +1300

    Forgot some files

---
+++ EasyBitLocker/EasyBitLockerMain/BitFolder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyBitLockerMain
+{
+	public class BitFolder
+	{
+		private static string targetDir;
+
+		// Constructor
+		public BitFolder(string path)
+		{
+			targetDir = path;
+		}
+
+
+		public static void CreateDir()
+		{
+			Directory.CreateDirectory(targetDir);
+
+		}
+						
+	}
+	public class test
+	{
+	}
+}
